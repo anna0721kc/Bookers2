97e735bc85bb99330cbebbacfbef7f98c5e5966a
anna0721kc <pipi0721s2@gmail.com>
2021-09-12 06:16:07 +0000

    [Finish] 4-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '6e23e495085a7fcf63f4cf70e82b1011b285fb835a0e25dab3afb6035a231dbcb45da33113cf12e1c6876d747b10af58bff965b7d14a8ac4790e0cb2ad595887'
